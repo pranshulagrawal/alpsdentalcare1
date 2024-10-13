2bed6ac3f889144504ab6c1dc5d0fbe697f51556
Pranshulagrawal <Pranshulagrawal9269@gmail.com>
2024-10-13 14:01:31 +0530

    add preloader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import AppRoutes from "./routes";
+import Preloader from "./components/Preloader"; // Assuming Preloader is in components folder
 
-function App() {
-  return <AppRoutes />;
-}
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 2000); // Delay for 2 seconds (2000ms)
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []);
+
+  return (
+    <>
+      {!isLoaded && <Preloader />} {/* Show Preloader while not loaded */}
+      {isLoaded && <AppRoutes />} {/* Show AppRoutes after loading */}
+    </>
+  );
+};
 
 export default App;
